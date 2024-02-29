d44d201b01077c16a6cb810e6c152deaeb400fa9
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-02-29 13:18:45 +0300

    Completed Task_3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -4,33 +4,46 @@
 Выведете его элементы, начиная с конца.
 Использовать рекурсию, не использовать циклы.
 */
-void Main ()
+void Main()
 {
-    int[] MyArray = GeterateLineArray(8, 10, 99);
+    int[] MyArray = GeterateLineArray(5, 10, 99);
+    Console.WriteLine("Исходный случайный массив:");
+    PrintArray(MyArray);
+    int count = MyArray.Length - 1;
+    Console.WriteLine("\nЭлементы масива в обратном порядке:");
+    PrintReversElemenArray(MyArray, count);
+
+
+    // Функции___________________________________
+
+
+    void PrintReversElemenArray(int[] array, int count)
+    {
+        if (count < 0)
+        {
+            return;
+        }
+        Console.Write(array[count] + " ");
+        PrintReversElemenArray(array, count - 1);
+        
+    }
     
-
-// Функции
-int[] GeterateLineArray (int size, int leftBound, int rightBound)
-{
-    int[] array = new int [size];
-    Random rnd = new Random();
-    for (int i = 0; i < size; i++)
+    
+    int[] GeterateLineArray(int size, int leftBound, int rightBound)
     {
-        array[i] = rnd.Next (leftBound, rightBound + 1);
+        int[] array = new int[size];
+        Random rnd = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rnd.Next(leftBound, rightBound + 1);
+        }
+        return array;
     }
-    return array;
-}
 
-void PrintArray (int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
+    void PrintArray(int[] array)
     {
-        Console.Write(array[i] + " ");
+        string strArray = string.Join(" ", array);
+        Console.Write($"[{strArray}]");
     }
-}
-
-
-
-
 }
 Main();
