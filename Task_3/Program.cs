3ee68df113da3fa59b3a4526caf09e759282139f
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-02-29 12:13:53 +0300

    Created a GenerareLineArray function

---
+++ Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача:
+Задайте произвольный массив.
+Выведете его элементы, начиная с конца.
+Использовать рекурсию, не использовать циклы.
+*/
+void Main ()
+{
+
+// Функции
+int[] GeterateLineArray (int size, int leftBound, int rightBound)
+{
+    int[] array = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next (leftBound, rightBound + 1);
+    }
+    return array;
+}
+}
+Main();
